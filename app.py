8a3e6f06201c977c35e8b890bef685618464ca25
adin65 <46980135+adin65@users.noreply.github.com>
2023-01-16 11:25:12 +0800

    added cors

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import tensorflow as tf
 
 # Import Flask API
 from flask import Flask, request
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 # Load the model
 model = tensorflow.keras.models.load_model('keras_model.h5')
 
