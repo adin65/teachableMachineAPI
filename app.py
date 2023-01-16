1e3cad9ee0a94094444d8a45e441566f3f4f40ff
Artina Sin <46980135+adin65@users.noreply.github.com>
2023-01-16 23:52:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,11 @@ def keras():
     else:
         #If the apikey is not the same, then return a 400 status indicating an error.
         return "Not valid apikey", 400 
-    
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!', 200
+
 #if __name__ == "__main__":
 #     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 # # Disable scientific notation for clarity
